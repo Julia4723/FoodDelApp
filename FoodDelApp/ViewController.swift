a97fc781fcae499325b2374c803d2a65e5d4b617
My <uk472363@yanex.ru>
2024-12-22 19:32:09 +0300

    Initial Commit

---
+++ FoodDelApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodDelApp
+//
+//  Created by user on 22.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
