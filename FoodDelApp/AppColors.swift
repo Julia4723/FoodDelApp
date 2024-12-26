6867acf43f86933c5d0ad5ef5152dc8b40bc685d
My <uk472363@yanex.ru>
2024-12-26 10:02:40 +0300

    add NavigationLayer

---
+++ FoodDelApp/AppColors.swift
@@ -0,0 +1,15 @@
+//
+//  AppColors.swift
+//  FoodDelApp
+//
+//  Created by user on 22.12.2024.
+//
+import UIKit
+
+enum AppColors {
+    static let accentColor = UIColor().hex(0xD35400)
+    static let grayBackground = UIColor().hex(0xECF0F1)
+    static let grayText = UIColor().hex(0x34495E)
+    static let blackText = UIColor().hex(0x000000)
+    static let green = UIColor().hex(0x2ECC71)
+}
